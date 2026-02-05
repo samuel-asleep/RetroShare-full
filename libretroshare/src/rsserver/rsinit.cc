3c3f0709dbaa49370c5cb2a7c826bbc1c16ed38e
Akinniranye Samuel Tomiwa <benneu40@gmail.com>
2026-02-05 18:13:48 +0100

    Fix login handler usage and gitignore

--- libretroshare/src/rsserver/rsinit.cc
+++ libretroshare/src/rsserver/rsinit.cc
@@ -2240,8 +2240,8 @@ bool RsLoginHelper::createLocation(
 	std::string sslPassword =
 	        RSRandom::random_alphaNumericString(RsInit::getSslPwdLen());
 
-	if(!RsNotify->cachePgpPassphrase(password)) return false;
-	if(!RsNotify->setDisableAskPassword(true)) return false;
+	if(!RsLoginHandler::cachePgpPassphrase(password)) return false;
+	if(!RsLoginHandler::setDisableAskPgpPassphrase(true)) return false;
 
 	bool ret = RsAccounts::createNewAccount(
 	            l.mPgpId, "", l.mLocationName, "", makeHidden, makeAutoTor,
@@ -2250,7 +2250,7 @@ bool RsLoginHelper::createLocation(
 	ret = ret && RsInit::LoadPassword(sslPassword);
 	ret = ret && RsInit::OK == attemptLogin(l.mLocationId, password);
 
-	RsNotify->setDisableAskPassword(false);
+	RsLoginHandler::setDisableAskPgpPassphrase(false);
 	return ret;
 }
 #endif // !RS_VERSION_AT_LEAST(0,6,6)
